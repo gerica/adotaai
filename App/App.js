0d4b3d164aaf0c6ff1e3d1feb5c276a5354f9c00
gerica <rogeriogerica@gmail.com>
2019-01-18 19:57:29 -0200

    consertando o style

--- App/App.js
+++ App/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Fonts from './Theme/Fonts';
+import ApplicationStyles from './Theme/ApplicationStyles';
 import configureStore from './Stores';
 import HomePage from './Containers/Home';
 // import configureStore from './utils/configureStore';
@@ -25,19 +27,19 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
+    ...ApplicationStyles.screen.container,
+    margin: 30,
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
+  title: {
+    ...Fonts.style.h2,
     textAlign: 'center',
-    margin: 10,
+    marginBottom: 10,
   },
-  instructions: {
+  text: {
+    ...Fonts.style.normal,
     textAlign: 'center',
-    color: '#333333',
     marginBottom: 5,
   },
-});
+})
